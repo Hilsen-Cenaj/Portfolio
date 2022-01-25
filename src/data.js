fe8a041ed79ad1be1ba603ebd00250621e69f3a3
Hilsen-Cenaj <christsenaj@gmail.com>
2022-01-26 00:30:34 +0200

    Add Neural Network project

--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 import Reversi from "./images/reversi.gif";
+import Neural from "./images/neural_net.gif"
 
 export const data = [
   {
@@ -8,6 +9,12 @@ export const data = [
     description:
       "The user can play a game called Reversi. He can choose the difficulty of the game with the depth option. The program has rules to determine the user's moves or if he has no moves and in the end declares a winner.",
   },
-  { id: 2, title: "", img: "", description: "" },
+  {
+    id: 2,
+    title: "Bitcoin Price Prediction Using Neural Network",
+    img: Neural,
+    description:
+      "The focus of the project is to implement a simple Neural Network to predict Bitcoin prices. First, we split (training and testing) and preprocess the data. Then, create the Neural network and test the model and finally, visualize the difference between the actual prices and the predicted prices",
+  },
   { id: 3, title: "", img: "", description: "" },
 ];
